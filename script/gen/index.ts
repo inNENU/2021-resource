563c1c5f4e24c8277b020870cc1823f55db8f0eb
Mr.Hope <zhangbowang1998@gmail.com>
2020-09-07 00:52:47 +0800

    update res

--- script/gen/index.ts
+++ script/gen/index.ts
@@ -1,4 +1,3 @@
-import { exec } from "child_process";
 import { sync as del } from "del";
 import { convertFolder } from "../util/yml2json";
 import { resolvePage } from "../components/page";
@@ -11,19 +10,19 @@ import { genSearchMap } from "./search";
 // import { genSitemap } from "./sitemap";
 import { resolveMarker } from "./marker";
 import { pushPages } from "./push";
-import { zip } from "./zip";
+import { genResource } from "./zip";
 // 删除旧的文件
 del([
   "./resource/function/**",
   "./resource/guide/**",
   "./resource/intro/**",
+  "./resource/icon/**",
   "./resource/other/**",
 ]);
 
 // 生成对应的 JSON
-convertFolder("./res/intro", "./resource/intro", (data, filePath) =>
-  resolvePage(data, `intro/${filePath}`)
-);
+
+// 功能大厅
 convertFolder("./res/function", "./resource/function", (data, filePath) =>
   filePath.match(/map\/marker\/(benbu|jingyue)/u)
     ? resolveMarker(data)
@@ -33,9 +32,17 @@ convertFolder("./res/function", "./resource/function", (data, filePath) =>
     ? genPEScore(data)
     : data
 );
+// 东师介绍
+convertFolder("./res/intro", "./resource/intro", (data, filePath) =>
+  resolvePage(data, `intro/${filePath}`)
+);
+
+// 东师指南
 convertFolder("./res/guide", "./resource/guide", (data, filePath) =>
   resolvePage(data, `guide/${filePath}`)
 );
+
+// 其他文件
 convertFolder("./res/other", "./resource/other", (data, filePath) =>
   resolvePage(data, `other/${filePath}`)
 );
@@ -53,47 +60,19 @@ genLyric();
 // genSitemap();
 count();
 
+// 生成 tab 页
 convertFolder("./res/config", "./resource/config", (data, filePath) =>
   filePath.match(/(function|guide|intro|main)/u)
     ? resolvePage(data, filePath)
     : data
 );
 
+// 生成资源
+genResource();
+
 // 生成二维码
 genQRCode()
   .then(() => pushPages())
   .then(() => {
-    exec("git add resource/");
-    exec("git diff --cached --name-status", (_err, gitDiffResult) => {
-      exec("git diff --name-status", (_err, gitDiffResult2) => {
-        // 功能配置有更新
-        if (
-          gitDiffResult.match(/resource\/function/u) ||
-          gitDiffResult2.match(/resource\/function/u)
-        )
-          zip("function");
-
-        // 指南配置有更新
-        if (
-          gitDiffResult.match(/resource\/guide/u) ||
-          gitDiffResult2.match(/resource\/guide/u)
-        )
-          zip("guide");
-
-        // 介绍配置有更新
-        if (
-          gitDiffResult.match(/resource\/intro/u) ||
-          gitDiffResult2.match(/resource\/intro/u)
-        )
-          zip("intro");
-
-        // 图标配置有更新
-        if (
-          gitDiffResult.match(/resource\/icon/u) ||
-          gitDiffResult2.match(/resource\/icon/u)
-        )
-          zip("icon");
-      });
-    });
     console.log("全部完成");
   });
